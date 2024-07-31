2c0af69d3bed8214c612486467bb382673a7740d
keshika18r <keshika.r@benhive.com>
2024-07-31 17:22:22 +0530

    commit with account models

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import DateTime, ForeignKey
 from app import db
 from datetime import datetime
  
@@ -16,24 +16,64 @@ class Roles(db.Model):
         db.session.add(self)
         db.session.commit()
 
+
+class policeDepartmentModel(db.Model):
+    
+    __tablename__ = 'police_department'
+
+    police_department_id = db.Column(db.Integer,primary_key=True)
+    county_id = db.Column(db.Integer)
+    name = db.Column(db.String(60))
+    code = db.Column(db.String(50))
+    login_link = db.Column(db.String(50),unique = True)
+    search_link = db.Column(db.String(50),unique = True)
+    created_date_time = db.Column(DateTime,default = datetime.now())
+    status = db.Column(db.Integer,default = 1)
+    is_enabled = db.Column(db.Integer,default = 1)
+    
+    def savePoliceDepartment(self):
+        db.create_all()
+        db.session.add(self)
+        db.session.commit()
+class Accounts(db.Model):
+    
+    __tablename__ = 'accounts'
+    
+    account_id = db.Column(db.Integer,primary_key=True)
+    police_department_id=db.Column(db.String(32), ForeignKey('police_department.police_department_id'))
+    police_dep_id = db.relationship('policeDepartmentModel', backref='accounts')
+    first_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60))
+    middle_name = db.Column(db.String(60))
+    email_id = db.Column(db.String(60))
+    phone_number = db.Column(db.String(60))
+    added_date_time = db.Column(DateTime,default = datetime.now())
+    is_deleted = db.Column(db.Integer,default=0)
+    status = db.Column(db.Integer,default=1)
+    
+    def saveAccounts(self):
+        db.create_all()
+        db.session.add(self)
+        db.session.commit()
+
 # models for user
 class Users(db.Model):
     
     __tablename__ = 'users'
     
     user_id = db.Column(db.Integer,primary_key=True)
-    
     # role id foreign key initialization
     role_id = db.Column(db.Integer, ForeignKey('roles.role_id'))
-    role = db.relationship('Roles', backref='users')
-    account_id=db.Column(db.String(32), ForeignKey('account.account_id'))
-    account = db.relationship('Accounts', backref='users')
+    role = db.relationship('Roles')
+    account_id=db.Column(db.String(32), ForeignKey('accounts.account_id'))
+    account = db.relationship('Accounts')
     username = db.Column(db.String(45))
     password = db.Column(db.String(600))
-    is_enable = db.Column(db.Integer,default=0)
+    is_enable = db.Column(db.Integer,default=1)
     status = db.Column(db.Integer,default=1)
     
     def save_to_users(self):
         db.create_all()
         db.session.add(self)
         db.session.commit()     
+    
