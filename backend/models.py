97212d92e7cf931138bd40c7596cc8ad508f9141
sheelarepo1 <145828768+sheelarepo1@users.noreply.github.com>
2024-08-02 12:24:49 +0530

    commit - Model For Crash Reports

--- backend/models.py
+++ backend/models.py
@@ -15,67 +15,67 @@ class Roles(db.Model):
         db.session.add(self)
         db.session.commit()
 
-
-class policeDepartmentModel(db.Model):
-    
+class PoliceDepartmentModel(db.Model):
     __tablename__ = 'police_department'
-
-    police_department_id = db.Column(db.Integer,primary_key=True)
+    police_department_id = db.Column(db.Integer, primary_key=True)
     county_id = db.Column(db.Integer)
     name = db.Column(db.String(60))
     code = db.Column(db.String(50))
-    login_link = db.Column(db.String(50),unique = True)
-    search_link = db.Column(db.String(50),unique = True)
-    created_date_time = db.Column(DateTime,default = datetime.now())
-    status = db.Column(db.Integer,default = 1)
-    is_enabled = db.Column(db.Integer,default = 1)
-    
+    login_link = db.Column(db.String(50), unique=True)
+    search_link = db.Column(db.String(50), unique=True)
+    created_date_time = db.Column(DateTime, default=datetime.now)
+    status = db.Column(db.Integer, default=1)
+    is_enabled = db.Column(db.Integer, default=1)
+
     def savePoliceDepartment(self):
         db.create_all()
         db.session.add(self)
         db.session.commit()
+
 class Accounts(db.Model):
-    
     __tablename__ = 'accounts'
-    
-    account_id = db.Column(db.Integer,primary_key=True)
-    police_department_id=db.Column(db.String(32), ForeignKey('police_department.police_department_id'))
-    police_dep_id = db.relationship('policeDepartmentModel', backref='accounts')
+    account_id = db.Column(db.Integer, primary_key=True)
+    police_department_id = db.Column(db.Integer, ForeignKey('police_department.police_department_id'))
+    police_dep_id = db.relationship('PoliceDepartmentModel', backref='accounts')
     first_name = db.Column(db.String(60))
     last_name = db.Column(db.String(60))
     middle_name = db.Column(db.String(60))
     email_id = db.Column(db.String(60))
     phone_number = db.Column(db.String(60))
-    added_date_time = db.Column(DateTime,default = datetime.now())
-    is_deleted = db.Column(db.Integer,default=0)
-    status = db.Column(db.Integer,default=1)
-    
+    added_date_time = db.Column(DateTime, default=datetime.now)
+    is_deleted = db.Column(db.Integer, default=0)
+    status = db.Column(db.Integer, default=1)
+
     def saveAccounts(self):
         db.create_all()
         db.session.add(self)
         db.session.commit()
 
-# models for users
+# Models for Users
 class Users(db.Model):
-    __tablename__="users"
+    __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
-    role_id=db.Column(db.Integer,unique=False)
-    username=db.Column(db.String(50), unique=False)
-    password=db.Column(db.String(100), unique=False)
-    is_enable=db.Column(db.Integer, unique=False)
-    status=db.Column(db.Integer, unique=False)
+    role_id = db.Column(db.Integer, ForeignKey('roles.role_id'))
+    role = db.relationship('Roles')
+    account_id = db.Column(db.Integer, ForeignKey('accounts.account_id'))
+    account = db.relationship('Accounts')
+    username = db.Column(db.String(45))
+    password = db.Column(db.String(600))
+    is_enable = db.Column(db.Integer, default=1)
+    status = db.Column(db.Integer, default=1)
 
-    def save_to_user(self):
+    def save_to_users(self):
         db.create_all()
         db.session.add(self)
-        db.session()
+        db.session.commit()
 
 #models for county
 class County(db.Model):
-    __tablename__="county"
-    county_id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100), unique=False)
-    status=db.Column(db.Integer,unique=False)
+    __tablename__ = 'county'
+    county_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False)
+    status = db.Column(db.Integer, unique=False)
+
     def save_to_county(self):
         db.create_all()
         db.session.add(self)
@@ -100,3 +100,28 @@ class Occupants(db.Model):
         db.create_all()
         db.session.add(self)
         db.session.commit()    
+        db.session.commit()
+
+class CrashReports(db.Model):
+    __tablename__='crash_reports'
+    report_id=db.Column(db.String(32),primary_key=True)
+    account_id=db.Column(db.String(32),ForeignKey('accounts.account_id'))
+    account_Details = db.relationship('Accounts')
+    police_department_id=db.Column(db.String(32),ForeignKey('police_department.police_department_id'))
+    police=db.relationship('PoliceDepartmentModel',backref='policeDetails')
+    report_number=db.Column(db.String(100))
+    crash_date=db.Column(DateTime)
+    location=db.Column(db.String(100))
+    county_id=db.Column(db.String(32),ForeignKey('county.county_id'))
+    county=db.relationship('County')
+    crash_severity=db.Column(db.Integer) 
+    no_of_occupants= db.Column(db.Integer)  
+    file_name=db.Column(db.String(250))
+    added_date=db.Column(DateTime, default=datetime.now)
+    added_date_time= db.Column(DateTime,default=datetime.now) 
+    status=db.Column(db.Integer)
+
+    def save_to_crash_reports(self):
+        db.create_all()
+        db.session.add(self)
+        db.session.commit()
