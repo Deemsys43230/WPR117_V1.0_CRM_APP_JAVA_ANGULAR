93ad7f27f966925201bfa896f1098deb8548ca5a
Ivaraj C <ivaraj.c@benhive.com>
2024-08-12 07:56:39 -0400

    Commit with app file
    Msg through for token expired

--- backend/app.py
+++ backend/app.py
@@ -22,11 +22,12 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = sqlconfig
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 
 CORS(app)
 db.init_app(app)
 jwt = JWTManager(app)
-# app.register_blueprint(user_blueprint)
 app.register_blueprint(Roles_blueprint, url_prefix='/role')
 app.register_blueprint(County_Blueprint,url_prefix='/county')
 app.register_blueprint(account_blueprint)
@@ -34,6 +35,8 @@ app.register_blueprint(police_blueprint)
 app.register_blueprint(user_blueprint,url_prefix='/user')
 app.register_blueprint(CrashReport_Blueprint,url_prefix='/crash_reports')
 app.register_blueprint(Occupants_Blueprint,url_prefix='/occupants')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
