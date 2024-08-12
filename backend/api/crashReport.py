6874375e710e9d3fa18b8cfc2358603cb719b207
sheelarepo1 <145828768+sheelarepo1@users.noreply.github.com>
2024-08-12 17:35:45 +0530

    commit - issue fix

--- backend/api/crashReport.py
+++ backend/api/crashReport.py
@@ -98,61 +98,82 @@ class CreateCrashReport(Resource):
 
 # Get All Crash Reports
 class GetAllCrashReports(Resource):
-    @role_required('ROLE_SUPER_ADMIN','ROLE_ADMIN','ROLE_USER')
+    @role_required('ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_USER')
     def post(self):
         requestDetails = request.get_json()
-        page = requestDetails.get('page')
-        itemsPerPage = requestDetails.get('itemsPerPage')
-        accountId=requestDetails.get('accountId')
-        reportNumber= requestDetails.get('reportNumber')
-        crashDate= requestDetails.get('crashDate')
-        firstName=requestDetails.get('firstName')
-        lastName= requestDetails.get('lastName')
-        location= requestDetails.get('location')
-        addedOnFromDate= requestDetails.get('addedOnFromDate')
-        addedOnToDate= requestDetails.get('addedOnToDate')
-        searchType= requestDetails.get('searchType')
-        reportType=requestDetails.get('reportType')
-        countyId= requestDetails.get('countyId')
-        policeDepartmentId=requestDetails.get('policeDepartmentId')
+        page = requestDetails.get('page', 1)
+        itemsPerPage = requestDetails.get('itemsPerPage', 10)
+        accountId = requestDetails.get('accountId')
+        reportNumber = requestDetails.get('reportNumber')
+        crashDate = requestDetails.get('crashDate')
+        firstName = requestDetails.get('firstName')
+        lastName = requestDetails.get('lastName')
+        location = requestDetails.get('location')
+        addedOnFromDate = requestDetails.get('addedOnFromDate')
+        addedOnToDate = requestDetails.get('addedOnToDate')
+        searchType = requestDetails.get('searchType')
+        reportType = requestDetails.get('reportType')
+        countyId = requestDetails.get('countyId')
+        policeDepartmentId = requestDetails.get('policeDepartmentId')
+
         query = CrashReports.query
         user = Users.query.filter_by(username=get_jwt_identity()).first()
-        if(reportType==1):
-           if user:
-                accountId = user.account_id     
-        if(reportType==2):
-                    accountDetail=Accounts.query.filter_by(account_id=user.account_id).first()
-                    if accountDetail:
-                        policeDepartmentId = accountDetail.police_department_id
-        if (accountId!="0"):
-            query = query.filter_by(account_id=accountId)
-        if (reportNumber!=""):
-            query = query.filter_by(report_number=reportNumber)
-        # Paginate crash reports
-        if(crashDate!=""):
-            query = query.filter_by(crash_date=crashDate)
-        if(firstName!=""):
+        
+        if reportType == 1 and user:
+            accountId = user.account_id
+        elif reportType == 2:
+            accountDetail = Accounts.query.filter_by(account_id=user.account_id).first()
+            if accountDetail:
+                policeDepartmentId = accountDetail.police_department_id
+
+        if accountId != "0":
+            query = query.filter(CrashReports.account_id == accountId)
+            print("accountId")
+
+        if reportNumber:
+            print("reportNumber")
+            query = query.filter(CrashReports.report_number == reportNumber)
+
+        if crashDate:
+            print("crashDate")
+            query = query.filter(CrashReports.crash_date == crashDate)
+
+        if firstName:
+            print("firstName")
             query = query.join(CrashReports.occupants).filter(Occupants.first_name.ilike(f'%{firstName}%'))
-        if(lastName!=""):
-            query = query.join(CrashReports.occupants).filter(Occupants.last_name.ilike(f'%{lastName}%'))  
-        if(location!=""):
-            query = query.filter(location.ilike(f'%{location}%'))
-        if(countyId!=""):
-            query = query.filter_by(county_id=countyId)
-        if(policeDepartmentId!="" and (policeDepartmentId) is not None):
+
+        if lastName:
+            query = query.join(CrashReports.occupants).filter(Occupants.last_name.ilike(f'%{lastName}%'))
+
+        if location:
+            print("location")
+            query = query.filter(CrashReports.location.ilike(f'%{location}%'))
+            print(query)
+
+        if countyId:
+            print("countyId")
+            query = query.filter(CrashReports.county_id == countyId)
+
+        if policeDepartmentId and policeDepartmentId is not None:
+            print("policeDepartmentId",policeDepartmentId)
             query = query.join(CrashReports.police).filter(PoliceDepartmentModel.police_department_id == policeDepartmentId)
-        if (addedOnFromDate!=""):
-                from_date = datetime.strptime(addedOnFromDate, '%Y-%m-%d')
-                query = query.filter(CrashReports.added_date >= from_date)
-        if (addedOnToDate!=""):
-                to_date = datetime.strptime(addedOnToDate, '%Y-%m-%d')
-                query = query.filter(CrashReports.added_date <= to_date)
+
+        if addedOnFromDate:
+            print("addedOnFromDate")
+            from_date = datetime.strptime(addedOnFromDate, '%Y-%m-%d')
+            query = query.filter(CrashReports.added_date >= from_date)
+
+        if addedOnToDate:
+            print("addedOnToDate")
+            to_date = datetime.strptime(addedOnToDate, '%Y-%m-%d')
+            query = query.filter(CrashReports.added_date <= to_date)
+
+        print(query)
         query = query.distinct()
         data = query.paginate(page=page, per_page=itemsPerPage, error_out=False)
+
         report_list = []
-        # Iterate through crash reports and add occupants
         for crash in data.items:
-            # Use the relationship to get occupants for the current crash report
             occupants_forms = [{
                 "occupants_id": occupant.occupants_id,
                 "report_id": occupant.report_id,
@@ -163,7 +184,7 @@ class GetAllCrashReports(Resource):
                 "sequence_no": occupant.sequence_no,
                 "status": occupant.status
             } for occupant in crash.occupants]
-            
+
             report_data = {
                 "report_id": crash.report_id,
                 "account_id": crash.account_id,
@@ -178,12 +199,12 @@ class GetAllCrashReports(Resource):
                 "added_date": crash.added_date,
                 "added_date_time": crash.added_date_time,
                 "status": crash.status,
-                "occupantsForms": occupants_forms  # Add occupants data here
+                "occupantsForms": occupants_forms
             }
-            
-            report_list.append(report_data)       
-        return jsonify({'data': report_list, 'status': True, 'total': data.total, 'pages': data.pages})
 
+            report_list.append(report_data)
+
+        return jsonify({'data': report_list, 'status': True, 'total': data.total, 'pages': data.pages})
 class GetCrashReportById(Resource):
     def get(self,id):
         data=CrashReports.query.filter_by(report_id=id).first()
